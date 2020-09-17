30694ba8dfe5a5f5a5f4622f7fd80fc26efc2fcf
Sergiy Nimerovsky <sergii.nimerovsky@gmail.com>
2020-09-17 21:45:43 +0300

    first commit

---
+++ src/www/precache-manifest.b24bf4396f6e421d57250acd07691b56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060b011d475b11fb69e9d626d3740200",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e562990440aaf753aa5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1e75d5d259d6b295b893",
+    "url": "/static/js/2.399d5628.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/static/js/2.399d5628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e562990440aaf753aa5",
+    "url": "/static/js/main.812a0725.chunk.js"
+  },
+  {
+    "revision": "e447a45596fabe9d4632",
+    "url": "/static/js/runtime-main.a814e782.js"
+  }
+]);
